9136e854573f1f04354899b7c97e636b61bed924
Nandini Kumar <90007466+k-nandini@users.noreply.github.com>
2023-11-06 15:34:07 -0500

    V2.3
    Signed-off-by: Nandini Kumar <90007466+k-nandini@users.noreply.github.com>

--- js/map.js
+++ js/map.js
@@ -1,6 +1,3 @@
-//
-// OpenLayers Map Script
-//
 document.addEventListener("DOMContentLoaded", function() {
     fetch('assets/data/data.json')
     .then(response => response.json())
@@ -8,10 +5,9 @@ document.addEventListener("DOMContentLoaded", function() {
         var unionSquareData = data.UnionSquareNoiseData.location;
         var noiseEvents = data.UnionSquareNoiseData.noiseEvents;
 
-        // Create the map OpenLayers
         var map = new ol.Map({
             target: 'map',
-            controls: ol.control.defaults({ 
+            controls: ol.control.defaults({
                 attribution: false,
                 rotate: false,
                 zoom: false
@@ -23,81 +19,106 @@ document.addEventListener("DOMContentLoaded", function() {
                 })
             ],
             view: new ol.View({
-                center: ol.proj.fromLonLat([unionSquareData.longitude, unionSquareData.latitude]), 
-                zoom: 19, 
-                rotation:  331 * (Math.PI / 180)
+                center: ol.proj.fromLonLat([unionSquareData.longitude, unionSquareData.latitude]),
+                zoom: 19,
+                rotation: 331 * (Math.PI / 180)
             })
         });
 
-        // Define a color map for event types
         var eventTypeColorMap = {
-            "Construction": [255, 102, 102], // Red
-            "Traffic": [178, 102, 178], // Purple
-            "Subway": [179, 183, 187], // Grey
-            "Street Vendors": [255, 195, 77], // Orange
-            "Nightlife": [255, 155, 210], // Pink
-            "People": [255, 252, 235], // Yellow
-            "City Services": [77, 77, 77] // Black 
+            "Construction": [255, 102, 102],
+            "Traffic": [178, 102, 178],
+            "Subway": [179, 183, 187],
+            "Street Vendors": [255, 195, 77],
+            "Nightlife": [255, 155, 210],
+            "People": [255, 252, 235],
+            "City Services": [77, 77, 77]
         };
 
-        // Create a vector source to hold all noise event features
         var vectorSource = new ol.source.Vector();
+        var originalFeatures = {};
+        var filterState = {
+            "Construction": true,
+            "Traffic": true,
+            "Subway": true,
+            "Street Vendors": true,
+            "Nightlife": true,
+            "People": true,
+            "City Services": true
+        };
 
-        // Iterate over all noise events and create circles for each location
         noiseEvents.forEach(function(noiseEvent) {
+            if (!originalFeatures[noiseEvent.type]) {
+                originalFeatures[noiseEvent.type] = [];
+            }
+
             noiseEvent.locations.forEach(function(noiseLocation) {
-                // Skip if longitude or latitude is missing
                 if (!noiseLocation.longitude || !noiseLocation.latitude) {
                     return;
                 }
 
                 var decibelLevel = noiseEvent.decibelLevel;
-                var baseColor = eventTypeColorMap[noiseEvent.type] || [0, 0, 0]; // Default to black if type not found
+                var baseColor = eventTypeColorMap[noiseEvent.type] || [0, 0, 0];
 
-                // Create three circles with different transparencies and radii
                 for (var i = 1; i <= 3; i++) {
                     var radiusMultiplier, transparency;
                     switch (i) {
                         case 1:
                             radiusMultiplier = 0.26;
-                            transparency = 0.4; // 100%
+                            transparency = 0.4;
                             break;
                         case 2:
                             radiusMultiplier = 0.40;
-                            transparency = 0.3; // 50%
+                            transparency = 0.3;
                             break;
                         case 3:
                             radiusMultiplier = 0.5;
-                            transparency = 0.1; // 20%
+                            transparency = 0.1;
                             break;
                     }
 
                     var radius = radiusMultiplier * decibelLevel;
-                    var color = 'rgba(' + baseColor.join(',') + ',' + transparency + ')'; // Construct the RGBA color
+                    var color = 'rgba(' + baseColor.join(',') + ',' + transparency + ')';
 
                     var noiseCircle = new ol.Feature({
                         geometry: new ol.geom.Circle(ol.proj.fromLonLat([parseFloat(noiseLocation.longitude), parseFloat(noiseLocation.latitude)]), radius)
                     });
 
-                    // Define the style for the circle
                     noiseCircle.setStyle(new ol.style.Style({
                         fill: new ol.style.Fill({
-                            color: color // Color based on event type and transparency
+                            color: color
                         })
                     }));
 
-                    // Add the circle feature to the vector source
-                    vectorSource.addFeature(noiseCircle);
+                    noiseCircle.set('type', noiseEvent.type);
+                    originalFeatures[noiseEvent.type].push(noiseCircle);
                 }
             });
         });
 
-        // Create a vector layer with the vector source and add it to the map
+        function updateEventVisibility() {
+            vectorSource.clear();
+            Object.keys(filterState).forEach(function(eventType) {
+                if (filterState[eventType]) {
+                    vectorSource.addFeatures(originalFeatures[eventType]);
+                }
+            });
+        }
+
+        document.querySelectorAll('#filter-controls input[type="checkbox"]').forEach(function(checkbox) {
+            checkbox.addEventListener('change', function() {
+                filterState[this.value] = this.checked;
+                updateEventVisibility();
+            });
+        });
+
         var vectorLayer = new ol.layer.Vector({
             source: vectorSource
         });
 
         map.addLayer(vectorLayer);
+
+        updateEventVisibility();
     })
     .catch(error => console.error('Error loading JSON data:', error));
 });
