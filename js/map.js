769566d36bfefcbf94cbc36396ff66af01ceae22
Nandini Kumar <90007466+k-nandini@users.noreply.github.com>
2023-11-07 05:19:47 -0500

    V4.2
    Signed-off-by: Nandini Kumar <90007466+k-nandini@users.noreply.github.com>

--- js/map.js
+++ js/map.js
@@ -12,7 +12,6 @@ async function loadNoiseData() {
 }
 
 function initializeMap(noiseData) {
-  mapboxgl.accessToken = mapboxAccessToken;
   const map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/kuman293/clonlclap009b01qofndpfsy0',
@@ -25,10 +24,13 @@ function initializeMap(noiseData) {
 
   map.on('load', () => {
     addNoiseEventLayers(map, noiseData.noiseEvents);
+    setupTimeSlider(map, noiseData.noiseEvents);
   });
 }
 
 function addNoiseEventLayers(map, noiseEvents) {
+  map._noiseEventLayers = {};
+
   noiseEvents.forEach((event, eventIndex) => {
     event.locations.forEach((location, locationIndex) => {
       const sourceId = `source-${eventIndex}-${locationIndex}`;
@@ -48,6 +50,78 @@ function addNoiseEventLayers(map, noiseEvents) {
   });
 }
 
+function addEventLayers(map, sourceId, eventTypeColor, eventIndex, locationIndex) {
+  const circles = [
+    { radiusMultiplier: 0.26, opacity: 0.5 },
+    { radiusMultiplier: 0.52, opacity: 0.3 },
+    { radiusMultiplier: 1.04, opacity: 0.1 }
+  ];
+
+  circles.forEach((circle, index) => {
+    const layerId = `layer-${eventIndex}-${locationIndex}-${index}`;
+    if (!map.getLayer(layerId)) {
+      map.addLayer({
+        'id': layerId,
+        'type': 'circle',
+        'source': sourceId,
+        'paint': {
+          'circle-radius': [
+            'interpolate', ['linear'], ['zoom'],
+            10, ['*', ['get', 'decibelLevel'], circle.radiusMultiplier],
+            18, ['*', ['get', 'decibelLevel'], circle.radiusMultiplier * 2]
+          ],
+          'circle-color': eventTypeColor,
+          'circle-opacity': circle.opacity
+        }
+      });
+
+      // Store the layer ID using the event ID for later reference
+      if (!map._noiseEventLayers[eventIndex]) {
+        map._noiseEventLayers[eventIndex] = [];
+      }
+      map._noiseEventLayers[eventIndex].push(layerId);
+    }
+  });
+}
+
+function setupTimeSlider(map, noiseEvents) {
+  const timeSlider = document.getElementById('timeSlider');
+  const timeSliderValue = document.getElementById('timeSliderValue');
+  
+  // Update the time display when the page loads
+  timeSliderValue.textContent = formatTime(timeSlider.value) + ':00';
+
+  timeSlider.addEventListener('input', () => {
+    const timeValue = timeSlider.value;
+    timeSliderValue.textContent = formatTime(timeValue) + ':00'; // Update the time display
+    updateMapForTime(map, noiseEvents, formatTime(timeValue));
+  });
+}
+
+function updateMapForTime(map, noiseEvents, sliderTime) {
+  noiseEvents.forEach((event, eventIndex) => {
+    const isVisible = isEventVisible(event, sliderTime);
+
+    const layerIds = map._noiseEventLayers[eventIndex];
+    if (layerIds) {
+      layerIds.forEach(layerId => {
+        map.setLayoutProperty(layerId, 'visibility', isVisible ? 'visible' : 'none');
+      });
+    }
+  });
+}
+
+function isEventVisible(event, sliderTime) {
+  return event.time.some(timeSlot =>
+    sliderTime >= timeSlot.startTime && sliderTime <= timeSlot.endTime
+  );
+}
+
+function formatTime(time) {
+  // Format the time as HH:MM:SS
+  return `${String(time).padStart(2, '0')}:00`;
+}
+
 function createFeatureData(location, decibelLevel) {
   return {
     'type': 'Feature',
@@ -61,32 +135,6 @@ function createFeatureData(location, decibelLevel) {
   };
 }
 
-function addEventLayers(map, sourceId, eventTypeColor, eventIndex, locationIndex) {
-  const circles = [
-    { radiusMultiplier: 0.26, opacity: 0.5 },
-    { radiusMultiplier: 0.52, opacity: 0.3 },
-    { radiusMultiplier: 1.04, opacity: 0.1 }
-  ];
-
-  circles.forEach((circle, index) => {
-    const layerId = `layer-${eventIndex}-${locationIndex}-${index}`;
-    map.addLayer({
-      'id': layerId,
-      'type': 'circle',
-      'source': sourceId,
-      'paint': {
-        'circle-radius': [
-          'interpolate', ['linear'], ['zoom'],
-          10, ['*', ['get', 'decibelLevel'], circle.radiusMultiplier],
-          18, ['*', ['get', 'decibelLevel'], circle.radiusMultiplier * 2]
-        ],
-        'circle-color': eventTypeColor,
-        'circle-opacity': circle.opacity
-      }
-    });
-  });
-}
-
 function getEventTypeColor(eventType) {
   const eventColors = {
     'Construction': '#FF6666',
@@ -97,7 +145,8 @@ function getEventTypeColor(eventType) {
     'People': '#FFFCEB',
     'City Services': '#4D4D4D'
   };
-  return eventColors[eventType] || '#AAAAAA';
+  return eventColors[eventType] || '#AAAAAA'; // Default color for better visibility
 }
 
+// Call the function to load and display the noise data
 loadNoiseData();
